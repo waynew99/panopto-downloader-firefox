b0ba6b81ec44a8fb6793489c6b3a6530f2fd7c09
Cliff Yang <cliffyyc@outlook.com>
2022-02-16 09:49:05 -0500

    removed find rss button, added loading screen and fixed reverse sequence, pending styling

--- popup.js
+++ popup.js
@@ -1,20 +1,16 @@
-let globalRes;  // to pass data between setContent and onSubmit
-
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById('findRSS').addEventListener('click', onclick, false)
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, "findRSS", setContent);
+  });
 
-  function onclick() {
-    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, "findRSS", setContent);
-    })
-  }
+  let resObj;  // to pass data between setContent and onSubmit
 
   function onSubmit() {
     let download = {}
-    for (const key in globalRes) {
+    for (const key in resObj) {
       const checkbox = document.getElementById(key.replace(/\s/g, ''));
       if (checkbox.checked) {
-        download[key] = globalRes[key];
+        download[key] = resObj[key];
       }
     }
     for (const name in download) {
@@ -26,13 +22,19 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function setContent(res) {
-    globalRes = res;
+    resObj = res;
     let content = "";
-    for (const key in res) {
-      content += `<input type="checkbox" id=${key.replace(/\s/g, '')} name=${key} value=2><label for=${key}> ${key}</label><br>`;
+
+    // reverse the sequence of the keys so the popup sequence matches that of the videos
+    let keys = Array.from(Object.keys(res)).reverse();
+
+    for (const index in keys) {
+      content += `<input type="checkbox" id=${keys[index].replace(/\s/g, '')} name=${keys[index]} value=2><label for=${keys[index]}> ${keys[index]}</label><br>`;
     }
     content += '<button id="submit" type="submit">Download Selected</button>';
-    document.body.innerHTML += content;
+
+    document.getElementById("loadScreen").innerHTML = "";
+    document.getElementById("checkList").innerHTML = content;
     document.getElementById('submit').addEventListener('click', onSubmit, false)
   }
 
