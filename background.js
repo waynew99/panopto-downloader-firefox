dd4c6bd2b0ba0283129ff31a3d3c8b48eff89d91
Wayne Wang <sw@middlebury.edu>
2022-02-15 21:41:25 -0500

    not working, but pushing

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
 let color = '#aaaaaa';
 
+//Hard coded RSS resource
+myRSSList = [
+  "abcde"
+];
+
+// Grab RSS list from the current webpage
+
+// Store RSS list into storage
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ myRSSList });
+  console.log('RSSList set with: ', myRSSList);
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ let changeColor = document.getElementById("changeColor");
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
+console.log("hihihihi");
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
@@ -22,3 +23,13 @@ function setPageBackgroundColor() {
     document.body.style.backgroundColor = color;
   });
 }
+
+// get the RSS list from the storage
+chrome.storage.sync.get("RSSList", ({ myRSSList }) => {
+  console.log(document.body.innerHTML);
+  console.log(myRSSList)
+  document.body.innerHTML += `<form><input type="checkbox" id="video1" name="video1"><label for="vehicle1"> ${myRSSList} </label><br><input type="submit" value="Submit"></form>`;
+  console.log(document.body.innerHTML);
+});
+
+// populate popup.html with the RSS list
