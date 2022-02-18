0ffd308bde395280e8064d8c5fcd2d236ce9ff08
Wayne Wang <sw@middlebury.edu>
2022-02-17 19:26:22 -0500

    added panopto url limitation

--- popup.js
+++ popup.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, "findRSS", setContent);
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    
+    if (url == undefined || !url.includes("panopto.com")) {
+      document.getElementById("loadScreen").innerHTML = "";
+      document.getElementById("result").innerHTML = '<p style="margin-bottom: 0px">No videos found.</p><p style="margin-top: 9px">Please navigate to the Panopto folder page containing the videos that you want to download.</p>';
+      return;
+    } else {
+      chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, "findRSS", setContent);
+      });
+    }
   });
 
   let resObj;  // to pass data between setContent and onSubmit
